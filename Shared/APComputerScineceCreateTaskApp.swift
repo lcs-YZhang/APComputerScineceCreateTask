95f2211b5c1ccad5a9ca06a92191f3b11f883678
Steven Zhang <YZhang@lcs.on.ca>
2022-03-25 11:20:27 -0400

    Initial Commit

---
+++ Shared/APComputerScineceCreateTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  APComputerScineceCreateTaskApp.swift
+//  Shared
+//
+//  Created by Steven Zhang on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct APComputerScineceCreateTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
